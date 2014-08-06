10560736095ce4245ba0a8a30e82bce59e7600cd
Steve Baker <Steven.Baker@adazzle.com>
2014-08-06 17:50:34 +0100

    Move imports into a seppcific examples.less file, play nice woth other build systems

--- gulp/tasks/less.js
+++ gulp/tasks/less.js
@@ -3,7 +3,7 @@ var less = require("gulp-less");
 
 // task
 gulp.task('styles', function () {
-    return gulp.src('./themes/bootstrap.less')
+    return gulp.src('./examples/bootstrap.less')
     .pipe(less())
     //compile into our examples folder
     .pipe(gulp.dest('./examples/build'));
