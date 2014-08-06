5b51e7c702ec64aef0f7c873c65e8ec61e028120
Steve Baker <Steven.Baker@adazzle.com>
2014-08-06 17:56:06 +0100

    fix spacing for if(s)

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -44,7 +44,7 @@ gulp.task('browserify', function() {
       .on('end', bundleLogger.end);
   };
 
-  if(global.isWatching) {
+  if (global.isWatching) {
     // Rebundle with watchify on changes.
     bundler.on('update', bundle);
   }
